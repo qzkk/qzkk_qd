a63d1d81c18d020c9ea6bead9a54c42031bce016
SanYi2019 <p_lifer@163.com>
2019-08-06 20:26:09 +0800

    物资申请前端表单提交功能

--- qzkk-master/src/store.js
+++ qzkk-master/src/store.js
@@ -8,18 +8,20 @@ export default new Vuex.Store({
     state: {
         ACCOUNT_NAME: localStorage.getItem("account"),
         ACCOUNT_TYPE:  localStorage.getItem("type"),
-        UID:  localStorage.getItem("uid"),
+        UID:  localStorage.getItem("uId"),
     },
 
     mutations: {
         setState(state, payload) {
+            console.log("))))");
+            console.log(payload);
             localStorage.setItem('account', payload.data.account);
             localStorage.setItem('type', payload.data.type);
-            localStorage.setItem('uid', payload.data.uid);
+            localStorage.setItem('uId', payload.data.uId);
 
             state.ACCOUNT_NAME = payload.data.account;
             state.ACCOUNT_TYPE = payload.data.type;
-            state.UID = payload.data.uid;
+            state.UID = payload.data.uId;
         }
     },
 
