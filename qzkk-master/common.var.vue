7883768e1b4baae776a5f49670ff3d6c273c9f94
Administrator <463948935@qq.com>
2019-08-24 18:15:56 +0800

    1.完善管理员查看物资功能  2.添加完成物资审核页面

--- qzkk-master/common.var.vue
+++ qzkk-master/common.var.vue
@@ -2,9 +2,9 @@
 </template>
 <script type="text/javascript">
     //本地开发
-    // var axiosServe='http://localhost:8080/api';
+    var axiosServe='http://localhost:8080/api';
     //部署配置
-    var axiosServe='http://219.244.186.2:8080/api';
+    // var axiosServe='http://219.244.186.2:8080/api';
     export default{
         axiosServe
     }
